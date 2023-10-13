b60df97dedce2082558af969046a53f27f32803c
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2023-10-13 14:46:06 +0300

    fix parser and buffer size

--- assignment2/command.h
+++ assignment2/command.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdio.h>
+#include <sys/types.h> 
 
-int execute_command(int last_expr, char* exe, int args_count, char** args, int input_fd, int output_fd);
-void execute_pipeline(const struct command_line *line);
+int execute_command(int first_expr, int last_expr, char* exe, int args_count, char** args, int input_fd, int output_fd, int* to_exit,
+    int* exit_code);
+void execute_commands(const struct command_line *line, int* to_exit, int* exit_code);
--- assignment2/solution.c
+++ assignment2/solution.c
@@ -5,83 +5,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <ctype.h>  
+#include <sys/types.h>
+#include <sys/wait.h>
 
-static bool contains_pipe(const struct command_line *line) {
-    const struct expr *e = line->head;
-
-    while (e != NULL) {
-        if (e->type == EXPR_TYPE_PIPE) {
-            return true;
-        }
-        e = e->next;
-    }
-
-    return false;
-}
-
-static void execute_command_line(int* to_exit, const struct command_line *line) {
+static void execute_command_line(int* to_exit, const struct command_line *line, int* exit_code) {
     assert(line != NULL);
-
-    if (contains_pipe(line)) {
-        execute_pipeline(line);
-    } else {
-        const struct expr *e = line->head;
-        int input_fd = STDIN_FILENO; // Default input
-
-        while (e != NULL) {
-            if (e->type == EXPR_TYPE_COMMAND) {
-                int output_fd = STDOUT_FILENO; // Default output
-
-                if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-                    output_fd = open(line->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-
-                    if (output_fd == -1) {
-                        perror("open");
-                        exit(EXIT_FAILURE);
-                    }
-                } else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-                    output_fd = open(line->out_file, O_WRONLY | O_APPEND, 0666);
-
-                    if (output_fd == -1) {
-                        perror("open");
-                        exit(EXIT_FAILURE);
-                    }
-                }
-
-                // Execute the current command with appropriate input and output
-                *to_exit = execute_command((e->next == NULL ? 1 : 0), e->cmd.exe, e->cmd.arg_count, e->cmd.args, input_fd, output_fd);
-
-                if (to_exit) {
-                    return;
-                }
-
-                // Close the output file descriptor if it was opened
-                if (output_fd != STDOUT_FILENO) {
-                    close(output_fd);
-                }
-            } else if (e->type == EXPR_TYPE_PIPE) {
-                assert(false);
-            } else if (e->type == EXPR_TYPE_AND) {
-                printf("\tAND\n");
-            } else if (e->type == EXPR_TYPE_OR) {
-                printf("\tOR\n");
-            } else {
-                assert(false);
-            }
-
-            e = e->next;
-        }
-    }
+    execute_commands(line, to_exit, exit_code);
 }
 
 int main(void) {
-    const size_t buf_size = 1024;
+    // freopen("output.txt", "w", stdout);
+    const size_t buf_size = 1;
     char buf[buf_size];
-    int rc;
+    int32_t rc;
     struct parser *p = parser_new();
     int to_exit = 0;
+    int exit_code = 0;
     while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
         parser_feed(p, buf, rc);
+        if (buf[0] == EOF)
+            break;
         struct command_line *line = NULL;
         while (true) {
             enum parser_error err = parser_pop_next(p, &line);
@@ -91,13 +35,14 @@ int main(void) {
                 printf("Error: %d\n", (int)err);
                 continue;
             }
-            execute_command_line(&to_exit, line);
+            execute_command_line(&to_exit, line, &exit_code);
             command_line_delete(line);
-            if (to_exit)
-                goto do_parse_del;
+            if (to_exit) {
+                parser_delete(p);
+                exit(exit_code);
+            }
         }
     }
-do_parse_del:
     parser_delete(p);
-    return 0;
+    return exit_code;
 }
